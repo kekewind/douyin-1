43e199c8e4ceeb3b953a158a27bf0a00fdf92488
John gao <1649868243@qq.com>
2021-08-10 14:01:00 +0800

    fix:重写,将一些函数写入utils.py脚本中

--- get_followers_data.py
+++ get_followers_data.py
@@ -1,13 +1,9 @@
 # 获取正在关注的人的所有上传视频
 import requests
-import re
-import xlwings as xw
-import pymysql
-app = xw.App(visible=False, add_book=False)
-wb = app.books.add()
+from utils import *
 
 
-def get_data(sec_uid, signature, max_cursor):
+def get_one_page_videos_data(sec_uid, signature, max_cursor):
     url = f'https://www.iesdouyin.com/web/api/v2/aweme/post/?sec_uid={sec_uid}&count=21&max_cursor={max_cursor}&aid=1128&_signature={signature}&dytk='
     headers = {
         'accept': 'application/json',
@@ -17,37 +13,28 @@ def get_data(sec_uid, signature, max_cursor):
     return response.json()
 
 
-def get_one_page(sec_uid, max_cursor):
-    page_data = []
+def get_one_page_info(sec_uid, max_cursor):
+    page_video_data = []
     signature = requests.get('http://localhost:3000/sign').text
-    data_json = get_data(sec_uid, signature, max_cursor)
+    data_json = get_one_page_videos_data(sec_uid, signature, max_cursor)
     if data_json and 'aweme_list' in data_json.keys():
-        aweme_list = data_json['aweme_list']
-        for item in aweme_list:
-            if item['aweme_type'] == 4:
-                aweme_id = item['aweme_id']
-                vid = item['video']['vid']
-                temp_src = item['video']['download_addr']['url_list'][0]
-                src = re.sub('&watermark=1', '&watermark=0', temp_src, re.S)
-                desc = re.sub('[\\/:*?"<>|\n]', '', item['desc'])
-                page_data.append([aweme_id, vid, desc, src])
-
+        page_video_data = data_json['aweme_list']
     try:
         iscontinue = data_json['has_more']
     except BaseException:
         print('keyerror')
     else:
         if iscontinue:
-            return page_data, data_json['max_cursor']
+            return page_video_data, data_json['max_cursor']
         else:
-            return page_data, None
+            return page_video_data, None
 
 
 def get_videos(sec_uid):
     max_cursor = 0
     all_data = []
     while True:
-        page_data, max_cursor = get_one_page(sec_uid, max_cursor)
+        page_data, max_cursor = get_one_page_info(sec_uid, max_cursor)
         all_data += page_data
         print(len(all_data))
         if not max_cursor:
@@ -55,78 +42,29 @@ def get_videos(sec_uid):
     return all_data
 
 
-def write2txt(datas, user):
-    with open(f'followers/{user}.txt', mode='w', encoding='utf-8') as f:
-        for video in datas:
-            f.write(video[0] + "==" + video[1] + "==" + video[2])
-            f.write('\n')
-
-
-def write2excel(data, user):
-    active_sheet = wb.sheets.add(user)
-    active_sheet.range("A:A").api.NumberFormat = "@"
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            active_sheet.range((i + 1, j + 1)).value = data[i][j]
-
-
-def connetmysql():
-    host = 'localhost'
-    port = 3306
-    database = 'douyin'
-    user = 'root'
-    password = '123456'
-    db = pymysql.connect(
-        host=host,
-        port=port,
-        db=database,
-        user=user,
-        password=password)
-    cursor = db.cursor()
-    sql = f'drop table if exists followers_datas'
-    cursor.execute(sql)
-    # page_data.append([aweme_id, vid, desc, src])
-    create_table = f"""CREATE TABLE followers_datas (
-                    author varchar(50) comment '作者',
-                    aweme_id varchar(25),
-                    vid varchar(100),
-                    description varchar(255),
-                    src varchar(255),
-                    primary key (aweme_id))"""
-    cursor.execute(create_table)
-    return db, cursor
-
-
-def write2mysql(datas, username, db, cursor):
-    for data in datas:
-        inser_sql = "insert into followers_datas values('{user}',{data})".format(
-            user=username, data=str(data)[1:-1])
-        try:
-            cursor.execute(inser_sql)
-            db.commit()
-        except BaseException:
-            db.rollback()
-
-
 if __name__ == "__main__":
     user_secid = []
+    # 获得数据库中已经写过的信息
+    mysql_data, mongodb_data = get_database_videos()
     with open('followers.txt', encoding='utf-8') as f:
         for item in f.readlines():
             user_secid.append(item.rstrip())
-    db, cursor = connetmysql()
     for item in user_secid:
         user, sec_id = item.split(':')
         print(user, sec_id)
         try:
-            user_data = get_videos(sec_id)
+            user_video_datas = get_videos(sec_id)
+            # 写入MySQL,excel,TXT中的数据与mongod的不一样
+            small_data = datas_process(user_video_datas)
         except Exception as e:
             print(e)
             print(user + "has error")
         else:
-            if len(user_data) > 0:
-                write2excel(user_data, user)
-                write2txt(user_data, user)
-                write2mysql(user_data, user, db, cursor)
+            if len(small_data) > 0:
+                write2excel(small_data, user)
+                write2txt(small_data, user)
+                write2mysql(small_data, user, db, cursor, mysql_data)
+                write2mongodb(user_video_datas, mongodb_data)
     cursor.close()
     db.close()
     del wb.sheets['Sheet1']
---
+++ utils.py
@@ -0,0 +1,107 @@
+import pymysql
+import re
+import xlwings as xw
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+database = client["douyin"]
+collection = database["followers_videos"]
+app = xw.App(visible=False, add_book=False)
+wb = app.books.add()
+host = 'localhost'
+port = 3306
+database = 'douyin'
+user = 'root'
+password = '123456'
+db = pymysql.connect(
+    host=host,
+    port=port,
+    db=database,
+    user=user,
+    password=password)
+cursor = db.cursor()
+
+
+def get_database_videos():
+    mysql_data = []
+    cursor.execute('select aweme_id from followers_datas')
+    for data in cursor.fetchall():
+        mysql_data.append(data[0])
+    datas = collection.find({'aweme_id': {'$ne': 1}})
+    mongodb_data = []
+    for data in datas:
+        mongodb_data.append(data['aweme_id'])
+    return mysql_data, mongodb_data
+
+
+def write2mongodb(user_videos_datas, mongodb_data):
+    for video in user_videos_datas:
+        aweme_id = video['aweme_id']
+        if aweme_id in mongodb_data:
+            pass
+        else:
+            try:
+                # 如果不考虑判断视频在不在的话,可以使用更新操作
+                # collection.update_one({'aweme_id': aweme_id}, {'$set': video})
+                collection.insert(video)
+            except BaseException:
+                print("插入mongodb数据库失败")
+
+
+def mysql_connect():
+    sql = f'drop table if exists followers_datas'
+    cursor.execute(sql)
+    create_table = f"""CREATE TABLE followers_datas (
+                    author varchar(50) comment '作者',
+                    aweme_id varchar(25),
+                    vid varchar(100),
+                    description varchar(255),
+                    src varchar(255),
+                    primary key (aweme_id))"""
+    cursor.execute(create_table)
+    return db, cursor
+
+
+def write2mysql(datas, username, db, cursor, mysql_data):
+    for data in datas:
+        if datas[0] in mysql_data:
+            pass
+        else:
+            inser_sql = "insert into followers_datas values('{user}',{data})".format(
+                user=username, data=str(data)[1:-1])
+            try:
+                cursor.execute(inser_sql)
+                db.commit()
+            except Exception as e:
+                db.rollback()
+
+
+def datas_process(userdata):
+    small_data = []
+    for item in userdata:
+        if item['aweme_type'] == 4:
+            aweme_id = item['aweme_id']
+            vid = item['video']['vid']
+            temp_src = item['video']['download_addr']['url_list'][0]
+            src = re.sub('&watermark=1', '&watermark=0', temp_src, re.S)
+            desc = re.sub('[\\/:*?"<>|\n]', '', item['desc'])
+            small_data.append([aweme_id, vid, desc, src])
+    return small_data
+
+
+def write2txt(datas, user):
+    with open(f'followers/{user}.txt', mode='w', encoding='utf-8') as f:
+        for video in datas:
+            f.write(video[0] + "==" + video[1] + "==" + video[2])
+            f.write('\n')
+
+
+def write2excel(data, user):
+    try:
+        active_sheet = wb.sheets.add(user)
+    except BaseException:
+        active_sheet = wb.sheets[user]
+    active_sheet.range("A:A").api.NumberFormat = "@"
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            active_sheet.range((i + 1, j + 1)).value = data[i][j]
