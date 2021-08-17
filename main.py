6dbaa3401ad9f6b3c5137b58a0b5b1ffdaf8c6d6
gaozp1123 <1649868243@qq.com>
2021-08-17 16:48:46 +0800

    fix:增加写入mongodb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # mitmdump抓包脚本
 import json
 import pymysql
+from pymongo import MongoClient
 host = 'localhost'
 port = 3306
 database = 'douyin'
@@ -12,12 +13,15 @@ db = pymysql.connect(
     db=database,
     user=user,
     password=password)
-
+client = MongoClient("mongodb://localhost:27017/")
+database = client["douyin"]
+collection = database["favorites"]
+cursor = db.cursor()
 
 def response(flow):
     # 将我的喜欢存入数据库和txt文件
-    if 'aweme/v1/aweme/favorite' in flow.request.url:
-        with open('favorite.txt', 'a', encoding='utf-8') as f:
+    if 'https://www.douyin.com/aweme/v1/web/aweme/favorite/' in flow.request.url:
+        with open('favorite2.txt', 'a', encoding='utf-8') as f:
             for aweme in json.loads(flow.response.text)['aweme_list']:
                 if 'download_addr' in aweme['video']:
                     f.write(
@@ -27,36 +31,36 @@ def response(flow):
                         "==" +
                         aweme['desc'])
                     f.write('\n')
-        cursor = db.cursor()
-        for aweme in json.loads(flow.response.text)['aweme_list']:
-            aweme_id = aweme['aweme_id']
-            aweme_type = aweme['aweme_type']
-            desc = aweme['desc']
-            download_addr = aweme['video']['play_addr']['uri']
-            create_time = aweme['create_time']
-            author_nickname = aweme['author']['nickname']
-            author_sec_uid = aweme['author']['sec_uid']
-            author_user_id = aweme['aweme_id']
-            downloaded = 0
-            data = [
-                aweme_id,
-                desc,
-                download_addr,
-                create_time,
-                aweme_type,
-                author_nickname,
-                author_sec_uid,
-                author_user_id,
-                downloaded]
-            sql = 'insert into favorites values ({data})'.format(
-                data=str(data)[1:-1])
-            print(sql)
-            try:
-                cursor.execute(sql)
-                db.commit()
-            except BaseException:
-                db.rollback()
-        cursor.close()
+                collection.insert_one(dict(aweme))
+                aweme_id = aweme['aweme_id']
+                aweme_type = aweme['aweme_type']
+                desc = aweme['desc']
+                download_addr = aweme['video']['play_addr']['uri']
+                create_time = aweme['create_time']
+                author_nickname = aweme['author']['nickname']
+                author_sec_uid = aweme['author']['sec_uid']
+                author_user_id = aweme['aweme_id']
+                downloaded = 0
+                data = [
+                    aweme_id,
+                    desc,
+                    download_addr,
+                    create_time,
+                    aweme_type,
+                    author_nickname,
+                    author_sec_uid,
+                    author_user_id,
+                    downloaded]
+                sql = 'insert into favorites values ({data})'.format(
+                    data=str(data)[1:-1])
+                print(sql)
+                try:
+                    cursor.execute(sql)
+                    db.commit()
+                except BaseException as e:
+                    print(e)
+                    db.rollback()
+            cursor.close()
 
     # 将关注的人存入txt文件
     # if 'aweme/v1/user/following/list' in flow.request.url:
